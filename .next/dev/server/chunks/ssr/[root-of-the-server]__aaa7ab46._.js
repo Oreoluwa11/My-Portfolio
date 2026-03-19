d3875de5f1401e71877f40d38c2f79e94bceb2ab
HP <HP@MacBook-Pro.local>
2026-03-19 14:26:28 +0100

    tabs

--- .next/dev/server/chunks/ssr/[root-of-the-server]__aaa7ab46._.js
+++ .next/dev/server/chunks/ssr/[root-of-the-server]__aaa7ab46._.js
@@ -1159,7 +1159,7 @@ const Resume = ()=>{
                             columnNumber: 11
                         }, ("TURBOPACK compile-time value", void 0)),
                         /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("a", {
-                            href: "/Anoba_Rahmat.Resume.pdf",
+                            href: "/Anoba_Rahmat_Frontend_Engineer.pdf",
                             download: "Anoba_Rahmat_Frontend_Engineer.pdf",
                             children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$components$2f$ui$2f$button$2e$tsx__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["Button"], {
                                 className: "btn-glow px-6 py-3 rounded-full",
